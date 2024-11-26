4ae1234bfad088c0d7d5fc5bf6120db0871548be
Zacky <zackyalhakim24@gmail.com>
2024-11-26 16:24:43 +0700

    revisi 2

--- src/server/server.js
+++ src/server/server.js
@@ -7,7 +7,7 @@ const InputError = require('../exceptions/InputError');
  
 (async () => {
     const server = Hapi.server({
-        port: 3000,
+        port: process.env.PORT || 3000,
         host: '0.0.0.0',
         routes: {
             cors: {
@@ -27,7 +27,7 @@ const InputError = require('../exceptions/InputError');
         if (response instanceof InputError) {
             const newResponse = h.response({
                 status: 'fail',
-                message: `${response.message} Silakan gunakan foto lain.`
+                message: `${response.message}`
             })
             newResponse.code(response.statusCode)
             return newResponse;
